170c569ef06aeac77a02e4262db9b5361534aa6a
kidvan777 <ivankidyaev.ik@gmail.com>
2017-09-12 13:27:37 +0300

    up

--- script.js
+++ script.js
@@ -1,29 +1,10 @@
 $(document).ready(function() {
-	
-	// Adding the x button
-	$(".ui-message").append('<span class="ui-close">X</span>');
-	
-	// Handling the click event
-	$(".ui-close").click(function() {
-		$(this).parents(".ui-message").fadeOut();
+// Position 
+	$(window).scroll(function(){
+
+		var windowScrollPosTop = $(window).scrollTop();
+		$(".status").html(windowScrollPosTop);
 	});
 	
-	// Initially hide the answers for the FAQ section
-	$(".answer").hide();
-	
-	// Handling the FAQ question click
-	$(".faq h4").click(function() {
-		
-		$(this).next(".answer").slideToggle();
-		
-	});
-	
-	$(".faq h4").addClass("faq-question");
-	
-	// Dynamically load about content
-	$("#about").click(function() {
-		$(".about-placeholder").load("about.html .body-text");
-		return false;
-	});
-	
+
 });
