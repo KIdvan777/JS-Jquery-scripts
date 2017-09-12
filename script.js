f132ff6d9684eee8aeb2a458cc4f8f49891d9e1f
kidvan777 <ivankidyaev.ik@gmail.com>
2017-09-12 14:28:30 +0300

    up

--- script.js
+++ script.js
@@ -1,9 +1,46 @@
 $(document).ready(function() {
-// Position 
+
+	var windowHeight = $(window).height();
+	var windowScrollPosTop = $(window).scrollTop();
+	var windowScrollPosBottom = windowHeight + windowScrollPosTop;
+
+	$.fn.revealOnScroll = function(){
+
+		return this.each(function(){
+
+			var objectOffset = $(this).offset();
+			var objectOffsetTop = objectOffset.top;
+				// $(".status").html(objectOffsetTop);
+
+			if(!$(this).hasClass("hidden")){
+				$(this).css("opacity",0).addClass("hidden");	
+			}
+
+			if(!$(this).hasClass("animaiton-complite")){
+
+				if (windowScrollPosBottom > objectOffsetTop) {
+
+				$(this).animate({"opacity":1},3000)
+				.addClass("animaiton-complite");
+
+				}
+			}
+		});
+
+	} //end of function
+
+
+
+	// Position 
 	$(window).scroll(function(){
 
-		var windowScrollPosTop = $(window).scrollTop();
-		$(".status").html(windowScrollPosTop);
+		windowHeight = $(window).height();
+		windowScrollPosTop = $(window).scrollTop();
+		windowScrollPosBottom = windowHeight + windowScrollPosTop;
+		
+		$(".sidebar").revealOnScroll();
+		$(".example-photo").revealOnScroll();
+		$("ul li").revealOnScroll();
 	});
 	
 
